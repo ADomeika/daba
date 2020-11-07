894d1208ae1c4d38662b10a4c9b6396144900ec8
Adomas Domeika <adomas.domeika@jet2.com>
2020-11-07 14:37:22 +0000

    Updates

--- precache-manifest.009c473198ec9d7fcbeea43e2a778640.js
+++ precache-manifest.009c473198ec9d7fcbeea43e2a778640.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "70839147cc03f59f3a4950372ee8d5ec",
+    "revision": "496db4359caafbac0b04fc032d4f0d85",
     "url": "/daba/index.html"
   },
   {
-    "revision": "4a05828af9bcc0025e18",
-    "url": "/daba/static/css/main.a1d33044.chunk.css"
+    "revision": "fc030d979bf74550867b",
+    "url": "/daba/static/css/main.3944d529.chunk.css"
   },
   {
-    "revision": "8138b45bf97904bf925f",
-    "url": "/daba/static/js/2.a5d6567f.chunk.js"
+    "revision": "1a04be008705e480eac7",
+    "url": "/daba/static/js/2.a64b0565.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/daba/static/js/2.a5d6567f.chunk.js.LICENSE.txt"
+    "url": "/daba/static/js/2.a64b0565.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4a05828af9bcc0025e18",
-    "url": "/daba/static/js/main.d6fc901a.chunk.js"
+    "revision": "fc030d979bf74550867b",
+    "url": "/daba/static/js/main.aee72105.chunk.js"
   },
   {
     "revision": "b548a1fc89e881fa4cfa",
