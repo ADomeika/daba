3541d5bb6952aebc06bf9cbf9a3e7f2eb3fd2d6d
Adomas Domeika <adomas.domeika@jet2.com>
2020-11-12 13:49:51 +0000

    Fixed desktop scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
@@ -6,40 +6,28 @@ import Bag from './svgs/Bag';
 
 const Header = ({ cartQuantity }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const stopScroll = () => {
-    if (!isOpen) {
+  useEffect(() => {
+    if (isOpen) {
       document.body.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = 'auto';
     }
-  }
+  }, [isOpen])
 
   return (
     <div className='header__inner'>
       <nav role='navigation' className={`menu${isOpen ? ' menu--open' : ''}`}>
         <ul>
-          <li className='menu__item' onClick={() => {
-            stopScroll()
-            setIsOpen(false)
-          }}>
+          <li className='menu__item' onClick={() => setIsOpen(false)}>
             <Link to='/' className='menu__link'>Pagrindinis</Link>
           </li>
-          <li className='menu__item' onClick={() => {
-            stopScroll()
-            setIsOpen(false)
-          }}>
+          <li className='menu__item' onClick={() => setIsOpen(false)}>
             <Link to='/produktai' className='menu__link'>Parduotuve</Link>
           </li>
-          <li className='menu__item' onClick={() => {
-            stopScroll()
-            setIsOpen(false)
-          }}>
+          <li className='menu__item' onClick={() => setIsOpen(false)}>
             <Link to='/apie' className='menu__link'>Apie Mus</Link>
           </li>
-          <li className='menu__item' onClick={() => {
-            stopScroll()
-            setIsOpen(false)
-          }}>
+          <li className='menu__item' onClick={() => setIsOpen(false)}>
             <Link to='/kontaktai' className='menu__link'>Kontaktai</Link>
           </li>
         </ul>
@@ -49,10 +37,7 @@ const Header = ({ cartQuantity }) => {
         <nav>
           <button
             className={`icon hamburger${isOpen ? ' open' : ''}`}
-            onClick={() => {
-              stopScroll()
-              setIsOpen(!isOpen)
-            }}
+            onClick={() => setIsOpen(!isOpen)}
           >
             <span></span>
             <span></span>
@@ -61,7 +46,7 @@ const Header = ({ cartQuantity }) => {
         </nav>
       </div>
       <div className='logo'>
-        <Link to='/' className='logo-link'>
+        <Link to='/produktai' className='logo-link'>
           <img src={require('../assets/images/logo192.png')} alt='DABA logo' className='logo-image' />
         </Link>
       </div>
